77a13c77827521074a54f9bb3ad41b04bde7ff50
Mariama-ny21 <mariama_ny21@yahoo.com>
2025-05-08 15:50:00 +0100

    Set up structure for game functions

---
+++ assets/js/script.js
@@ -0,0 +1,27 @@
+function runGame() {
+
+}
+
+function checkAnswer() {
+
+}
+
+function calculateCorrectAnswer(){
+
+}
+
+function incrementWrongScore(){
+
+}
+
+function displayAdditionQuestion(){
+
+}
+
+function displaySubtractQuestion(){
+
+} 
+
+function displayMultiplyQuestion(){
+    
+}
